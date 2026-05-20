ef397fe28f4c046e14c1989e83b626ef19ce3a1b
Navin1012 <nc13760@gmail.com>
2026-05-20 16:04:24 +0530

    first commit

---
+++ application/config/payroll.php
@@ -0,0 +1,58 @@
+<?php
+
+$config['staffattendance'] = array(
+    'present' => 1,
+    'half_day' => 4,
+    'late' => 2,
+    'absent' => 3,
+    'holiday' => 5
+);
+
+$config['contracttype'] = array(
+    'permanent' => 'Permanent',
+    'probation' => 'Probation',
+);
+
+$config['status'] = array(
+    'approve' => 'Approve',
+    'disapprove' => 'Disapprove',
+    'pending' => 'Pending',
+);
+
+$config['marital_status'] = array(
+    'Single' => 'Single',
+    'Married' => 'Married',
+    'Widowed' => 'Widowed',
+    'Seperated' => 'Seperated',
+    'Not Specified' => 'Not Specified',
+);
+
+$config['payroll_status'] = array(
+    'generated' => 'Generated',
+    'paid' => 'Paid',
+    'unpaid' => 'Unpaid',
+    'not_generate' => 'Not Generated',
+);
+$config['payment_mode'] = array(
+    'cash' => 'Cash',
+    'cheque' => 'Cheque',
+    'online' => 'Transfer to Bank Account',
+);
+$config['enquiry_status'] = array(
+    'active' => 'Active',
+    'passive' => 'Passive',
+    'dead' => 'Dead',
+    'won' => 'Won',
+    'lost' => 'Lost',
+);
+
+$config['work_shift'] = array(
+    'Morning Shift' => 'Morning Shift',
+    'Noon Shift'    => 'Noon Shift',
+    'General Shift' => 'General Shift',
+);
+
+$config['work_location'] = array(
+    'Kadodara' => 'Kadodara',
+    'Kadodara - 1' => 'Kadodara - 1'
+);
